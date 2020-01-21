97d7f154cb3d44df146888a03e12a4a0f7d8770a
alexpikuta <olexandr.pikuta@gmail.com>
2020-01-21 15:02:35 +0200

    Create layouts

---
+++ src/views/DetailRecord.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="app-content">
+    <div class="app-page">
+      <div>
+        <div>
+          <div class="breadcrumb-wrap">
+            <a
+              href="/history"
+              class="breadcrumb"
+            >History</a>
+            <a class="breadcrumb">
+              Outgo
+            </a>
+          </div>
+          <div class="row">
+            <div class="col s12 m6">
+              <div class="card red">
+                <div class="card-content white-text">
+                  <p>Description:</p>
+                  <p>Amount:</p>
+                  <p>Category:</p>
+
+                  <small>12.12.12</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
---
+++ src/views/Record.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="app-page">
+    <div>
+      <div class="page-title">
+        <h3>New record</h3>
+      </div>
+
+      <form class="form">
+        <div class="input-field">
+          <select>
+            <option>
+              name cat
+            </option>
+          </select>
+          <label>Choose category</label>
+        </div>
+
+        <p>
+          <label>
+            <input
+              class="with-gap"
+              name="type"
+              type="radio"
+              value="income"
+            >
+            <span>Income</span>
+          </label>
+        </p>
+
+        <p>
+          <label>
+            <input
+              class="with-gap"
+              name="type"
+              type="radio"
+              value="outcome"
+            >
+            <span>Outgo</span>
+          </label>
+        </p>
+
+        <div class="input-field">
+          <input
+            id="amount"
+            type="number"
+          >
+          <label for="amount">Amount</label>
+          <span class="helper-text invalid">amount</span>
+        </div>
+
+        <div class="input-field">
+          <input
+            id="description"
+            type="text"
+          >
+          <label for="description">Description</label>
+          <span
+            class="helper-text invalid"
+          >description</span>
+        </div>
+
+        <button
+          class="btn waves-effect waves-light"
+          type="submit"
+        >
+          Create
+          <i class="material-icons right">send</i>
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
